10b70e62afde2255ae06f8db6937e94d6964f6df
Jennifer Stewart <jstewart8053@gmail.com>
2023-10-04 10:44:23 -0400

    initial commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Hero() {
+    return(
+        <div>
+            <img src="/assets/Home.gif" className="hero"/>
+            <p className="heroText">Do you ever wish you could accomplish activities, workshops, and inter-activities at your own pace, and in a beautiful and natural setting? A home away from home. Well now you can, with Staycation!</p>
+            <button>Show me more!</button>
+        </div>
+    )
+}
+
+export default Hero;
